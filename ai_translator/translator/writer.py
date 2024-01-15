51fb4aec1194514ad59216e56e4fa00fbb7c5a86
qingdengyue <zrl412@163.com>
2024-01-15 18:07:52 +0800

    update

--- ai_translator/translator/writer.py
+++ ai_translator/translator/writer.py
@@ -30,4 +30,71 @@ class Writer:
         LOG.info(f"pdf_file_path:{book.pdf_file_path}")
         LOG.info(f"开始翻译:{output_file_path}")
         
+
+        font_path="../fonts/alijiankangti45r.ttf"
+        pdfmetrics.registerFont(TTFont("alijiankangti45r",font_path))
+        
+        simsum_style=ParagraphStyle('alijiankangti45r',fontName='alijiankangti45r',fontSize=32,leading=14)
+        doc=SimpleDocTemplate(output_file_path,pagesize=pagesizes.letter)
+        styles=getSampleStyleSheet()
+        story=[]
+
+
+        for page in book.pages:
+            for content in page.contents:
+                if content.status:
+                    if content.content_type==ContentType.TEXT:
+                        text = content.translation
+                        para = Paragraph(text,simsum_style)
+                        story.append(para)
+                    elif content.content_type == ContentType.TABLE:
+                        table = content.translation
+                        table_style =TableStyle([
+                             ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                            ('FONTNAME', (0, 0), (-1, 0), 'alijiankangti45r'),  # 更改表头字体为 "SimSun"
+                            ('FONTSIZE', (0, 0), (-1, 0), 14),
+                            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                            ('FONTNAME', (0, 1), (-1, -1), 'alijiankangti45r'),  # 更改表格中的字体为 "SimSun"
+                            ('GRID', (0, 0), (-1, -1), 1, colors.black)
+                        ])  
+
+                        pdf_table=Table(table.values.tolist())  
+                        pdf_table.setStyle(table_style)
+                        story.append(pdf_table)
+            if page != book.pages[-1]:
+                story.append(PageBreak())
+        
+        doc.build(story)
+        LOG.info(f"翻译完成:{output_file_path}")
+    
+
+    def _save_translated_book_markdown(self,book:Book,output_file_path:str =None):
+        if output_file_path is None:
+            output_file_path = book.pdf_file_path.replace('.pdf',f'_translated.md')
+        
+        LOG.info(f"pdf_file_path:{book.pdf_file_path}")
+        LOG.info(f"开始翻译:{output_file_path}")
+        with open(output_file_path,'w',encoding='utf-8') as output_file:
+            for page in book.pages:
+                for content in page.contents:
+                    if content.status:
+                        if( content.content_type == ContentType.TEXT):
+                            text = content.translation
+                            output_file.write(text+"\n\n")
+                        
+                        elif content.content_type == ContentType.TABLE:
+                             # Add table to the Markdown file
+                            table = content.translation
+                            header = '| ' + ' | '.join(str(column) for column in table.columns) + ' |' + '\n'
+                            separator = '| ' + ' | '.join(['---'] * len(table.columns)) + ' |' + '\n'
+                            # body = '\n'.join(['| ' + ' | '.join(row) + ' |' for row in table.values.tolist()]) + '\n\n'
+                            body = '\n'.join(['| ' + ' | '.join(str(cell) for cell in row) + ' |' for row in table.values.tolist()]) + '\n\n'
+                            output_file.write(header + separator + body)
+            if page != book.pages[-1]:
+                output_file.write('---\n\n')
+        LOG.info(f"翻译完成:{output_file_path}")
+
             
