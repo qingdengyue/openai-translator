305c072ae9923d3e7d4eb0b02638252693ab5d18
ruilong.zhao <ruilong.zhao@dmall.com>
2024-02-09 16:07:14 +0800

    feat:update

--- webui/translator/views.py
+++ webui/translator/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import  FileSpeedLimitedConfiguration
+from django.template import loader
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello.You are index page.")
+    file_speed_limited_configuration_list=FileSpeedLimitedConfiguration.objects.order_by("id")[:2]
+    template=loader.get_template("translator/index.html")
+    context={
+        "file_speed_limited_configuration_list":file_speed_limited_configuration_list
+    }
+    return HttpResponse(template.render(context,request))
