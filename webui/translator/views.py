08da59a4e17a44a00470ba11b355477f006685f3
ruilong.zhao <ruilong.zhao@dmall.com>
2024-02-09 16:22:13 +0800

    feat:update

--- webui/translator/views.py
+++ webui/translator/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import  FileSpeedLimitedConfiguration
-from django.template import loader
+from django.shortcuts import render,get_object_or_404
 # Create your views here.
 def index(request):
     file_speed_limited_configuration_list=FileSpeedLimitedConfiguration.objects.order_by("id")[:2]
-    template=loader.get_template("translator/index.html")
     context={
         "file_speed_limited_configuration_list":file_speed_limited_configuration_list
     }
-    return HttpResponse(template.render(context,request))
+    return render(request,"translator/index.html",context)
+
+def detail(request,config_id):
+    try:
+      configuration=FileSpeedLimitedConfiguration.objects.get(pk=config_id)
+    except FileSpeedLimitedConfiguration.DoesNotExist:
+        raise Http404("Configuration does not exist")
+    return render(request,"translator/detail.html",{"configuration":configuration})
