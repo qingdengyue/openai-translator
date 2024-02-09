0d84d2a014ee5fafb0ca6fcc87ffd1bb5f28a8f8
ruilong.zhao <ruilong.zhao@dmall.com>
2024-02-09 14:25:11 +0800

    feat:update

--- webui/translator/admin.py
+++ webui/translator/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Welcome,FileSpeedLimitedConfiguration,FileUploadNumberRecord,SiteUser
+from .models import Welcome, FileSpeedLimitedConfiguration, FileUploadNumberRecord, SiteUser
 
 admin.site.register(Welcome)
 admin.site.register(FileSpeedLimitedConfiguration)
--- webui/webui/urls.py
+++ webui/webui/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
 
 urlpatterns = [
-    path('translator/',include('translator.urls')),
+    path('translator/', include('translator.urls')),
     path('admin/', admin.site.urls),
 ]
